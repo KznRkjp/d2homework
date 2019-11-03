1af79acbfb693236ee9c25a023e775645a9feb02
KznRkjp <46960704+KznRkjp@users.noreply.github.com>
2019-11-03 10:56:14 +0300

    11

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import os
 import sentry_sdk
 from bottle import Bottle, request
 from sentry_sdk.integrations.bottle import BottleIntegration
@@ -27,6 +28,6 @@ def index():
 
 
 if os.environ.get("APP_LOCATION") == "heroku":
-    app.run(host="localhost", int(os.environ.get("PORT", 8080), debug=False))
+    app.run(host="0.0.0.0", port = int(os.environ.get("PORT", 5000), debug=False))
 else:
     app.run(host="localhost", port=8080, debug=True)
